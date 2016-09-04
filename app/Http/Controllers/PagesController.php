43c1067acb05728f76bafa71c45eb8c95c1cdd3b
Adam Weeks <aoweeks@gmail.com>
2016-09-04 23:25:06 +0000

    Add About function to PagesController

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -5,6 +5,13 @@ namespace App\Http\Controllers;
 class PagesController extends Controller
 {
     public function home(){
+            
+        $people = ['Taylor', 'Matt', 'Jeffrey'];
         
+        return view('welcome', compact('people'));
+    }
+    
+    public function about(){
+        return view('pages.about');
     }
 }
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -13,14 +13,9 @@
 
 /*Route::get('/', function () {
     
-    $people = ['Taylor', 'Matt', 'Jeffrey'];
-    
-    return view('welcome', compact('people'));
     
 });*/
 
 Route::get('/', 'PagesController@home');
 
-Route::get('about', function() {
-    return view('pages.about');
-});
+Route::get('about', 'PagesController@about');
