da88d4b29c51fbdd7a0f85aceeaa2b982729b66d
Adam Weeks <aoweeks@gmail.com>
2016-09-05 22:47:13 +0000

    Add some Bootstrap styling

--- resources/views/cards/show.blade.php
+++ resources/views/cards/show.blade.php
@@ -1,14 +1,22 @@
 @extends('layout')
 
 @section('content')
-    <h1>{{ $card->title }}</h1>
-    <h3>{{ $card->created_at }}</h3ex>
-    
-    <ul>
-    @foreach ($card->$cnotes as $note)
+
+    <div class="row">
         
-        <li>{{ $note->body }}</li>
-        
-    @endforeach
-    </ul>
+        <div class="col-md-6 col-md-offset-3">
+            <h1>{{ $card->title }}</h1>
+            <h3>{{ $card->created_at }}</h3>
+            
+            <ul class="list-group">
+            @foreach ($card->notes as $note)
+                
+                <li class="list-group-item">{{ $note->body }}</li>
+                
+            @endforeach
+            </ul>
+            
+            <h3>Add a New Note</h3>
+        </div>
+    </div>
 @stop
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,12 +5,13 @@
     <title>Document</title>
     
     <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-    <link rel="stylesheet" href="/css/app.css"  />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css" />
     @yield('header')
 </head>
 <body>
-    
-    @yield('content')
+    <div class="container">
+        @yield('content')
+    </div>
 
     @yield('footer')
 </body>
