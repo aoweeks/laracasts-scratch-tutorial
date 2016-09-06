25d4d0861e149975cddb95c6bf0d34516149e9a9
aoweeks <aoweeks@gmail.com>
2016-09-06 15:24:32 +0000

    Add link to card show page to take user to note edit page

--- resources/views/cards/show.blade.php
+++ resources/views/cards/show.blade.php
@@ -11,7 +11,7 @@
             <ul class="list-group">
                 @foreach ($card->notes as $note)
                     
-                    <li class="list-group-item">{{ $note->body }}</li>
+                    <li class="list-group-item"><a href="/notes/{{ $note->id }}/edit">{{ $note->body }}</a></li>
                     
                 @endforeach
             </ul>
